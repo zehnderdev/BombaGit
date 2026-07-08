f98239941584d0bb4918fe4290a5b862e74e9b52
Joshua Zehnder <zehnder.dev@gmail.com>
2026-07-08 14:53:39 +0200

    refactored init to have no global variables

--- src/main.c
+++ src/main.c
@@ -14,18 +14,19 @@ int main(int argc, char *argv[]){
     // if init command -> create ".bgit" file
     if(argc <2) return 1; // check if we have 2 arguments at least (first always filename)
     char *command = argv[1];
-    char *suffix = "/.bgit"; // we call it bgit for now
-
-    char cwd[64]; //hopefully more than enough
-    getcwd(cwd,sizeof(cwd)); 
-
-    char *filepath = malloc(strlen(suffix)+strlen(cwd)+1); // allocate memory
-    if(!filepath)return 1; // check for malloc 
-
-    strcpy(filepath,cwd);
-    strcat(filepath,suffix);
-
+    
     if(strcmp(command,"init") == 0){
+        char *suffix = "/.bgit"; // we call it bgit for now
+
+        char cwd[64]; //hopefully more than enough
+        getcwd(cwd,sizeof(cwd)); 
+
+        char *filepath = malloc(strlen(suffix)+strlen(cwd)+1); // allocate memory
+        if(!filepath)return 1; // check for malloc 
+
+        strcpy(filepath,cwd);
+        strcat(filepath,suffix);
+
         int status;
         if(mkdir(filepath,0755)<0){ // normal permission settings
             if(errno == EACCES){
@@ -38,6 +39,6 @@ int main(int argc, char *argv[]){
         } 
 
         printf("initialize Repo");
+        free(filepath); // free memory
     }
-    free(filepath); // free memory
 }
