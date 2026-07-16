2bc45d8e89663cfd4540c093a5475221d41c6a53
Joshua Zehnder <zehnder.dev@gmail.com>
2026-07-16 21:41:06 +0200

    added fixes to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e # Ends on errors
 repo=https://github.com/zehnderdev/BombaGit
 
 dir=BombaGit
@@ -14,18 +15,21 @@ if [ -f Makefile ] && [ -d src ]
   else
     echo "Cloning repository"
     git clone $repo
-    cd $dir	
+    cd $dir || exit 1;	
 fi
 
+echo "Checking dependencies"
 # Check libssl-dev package and gcc for safety
 # command to check if user has distro
-if [ command -v apt &> /dev/null ] # ubuntu
+if command -v apt &> /dev/null  # ubuntu
   then
     sudo apt install -y gcc libssl-dev
-  elif [ command -v dnf &> /dev/null ] # Fedora / redhat
+  elif command -v dnf &> /dev/null  # Fedora / redhat
+  then
     sudo dnf install -y gcc openssl-devel 
-  elif [ command -v pacman &> /dev/null ] # arch
-    sudo pacman -S -noconfirm gcc openssl
+  elif command -v pacman &> /dev/null  # arch
+  then  
+    sudo pacman -S --needed --noconfirm gcc openssl
   else
     echo "Unsupported package manager"
     echo "Follow github page for instructions"
